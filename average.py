27799131741c090bbc6fbc83136f9102c440cd47
Skyeler Wakefield <wakefies@oregonstate.edu>
2021-04-07 19:34:20 -0400

    This program asks user to submit 5 numbers and then program will calculate average for user.

--- average.py
+++ average.py
@@ -2,15 +2,10 @@
 # Date: 4/7/21
 # Description:Write a program that asks the user for five numbers and then prints out the average of those numbers.
 
-print("Please enter five numbers: ")
-num_1 = -2.4
-num_2 = 5.1
-num_3 = 6.0
-num_4 = 123.8
-num_5 = -19.0
-print(num_1)
-print(num_2)
-print(num_3)
-print(num_4)
-print(num_5)
+print("Please enter five numbers.")
+num_1 = float(input())
+num_2 = float(input())
+num_3 = float(input())
+num_4 = float(input())
+num_5 = float(input())
 print("The average of those numbers is:", (num_1 + num_2 + num_3 + num_4 + num_5)/5)
